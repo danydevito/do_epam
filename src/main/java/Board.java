ab051ca995ed7a47fbfdd56f57cc3905a60ab696
danydevito <daniel1421@vp.pl>
2017-05-22 14:03:15 +0200

    wersja minimalna

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -24,6 +24,23 @@ public class Board {
     }
     String cell(){
         String answer = scanner.nextLine();
-        return answer;
+        answer = answer.toLowerCase();
+        if (answer.equals("a1") || answer.equals("a2") || answer.equals("a3") || answer.equals("b1") || answer.equals("b2")
+                || answer.equals("b3") || answer.equals("c1") || answer.equals("c2") || answer.equals("c3")) return answer;
+        else return "zla wspolrzedna";
+    }
+
+    public void winBoard(String[][] cells, String message1, String message2){
+        System.out.println("     A   B   C");
+        System.out.println("    --- --- --- ");
+        System.out.println(" 1 | "+cells[0][0]+" | "+cells[0][1]+" | "+cells[0][2]+" |");
+        System.out.println("    --- --- ---");
+        System.out.println(" 2 | "+cells[1][0]+" | "+cells[1][1]+" | "+cells[1][2]+" |");
+        System.out.println("    --- --- ---");
+        System.out.println(" 3 | "+cells[2][0]+" | "+cells[2][1]+" | "+cells[2][2]+" |");
+        System.out.println("    --- --- ---");
+        System.out.println();
+        System.out.println(message1);
+        System.out.println(message2);
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,10 +3,11 @@
  */
 public class Game {
 
-    int x,y;
+    int x,y, scoreX, scoreY;
     int playerNo;
     boolean end = false;
     String[][] cells = new String[3][3];
+    String message, score;
     Board board = new Board();
     Player playerX = new Player();
     Player playerO = new Player();
@@ -34,37 +35,47 @@ public class Game {
             board.drawBoard(cells, playerX.message1, playerO.message2);
             translator(board.cell());
             playerX.fields[x][y] = new String("X");
-            for (int i=0; i<3; i++){
-                for (int j=0; j<3; j++){
-                    System.out.print(playerX.fields[i][j]);
-                }
-                System.out.println();
-            }
+//            for (int i=0; i<3; i++){
+//                for (int j=0; j<3; j++){
+//                    System.out.print(playerX.fields[i][j]);
+//                }
+//                System.out.println();
+//            }
             end = playerX.isWin(playerX.fields);
+            if (end) scoreX++;
             cells[x][y] = "X";
-            playerNo--;
-            board.drawBoard(cells, playerX.message2, playerO.message1);
-            translator(board.cell());
-            playerO.fields[x][y] = new String("O");
-            end = playerO.isWin(playerO.fields);
-            cells[x][y] = "O";
+            if (!end) {
+                playerNo--;
+                board.drawBoard(cells, playerX.message2, playerO.message1);
+                translator(board.cell());
+                playerO.fields[x][y] = new String("O");
+                end = playerO.isWin(playerO.fields);
+                if (end) scoreY++;
+                cells[x][y] = "O";
+            }
 
         }while (!end);
+
+        if (playerNo==1) message = "Zwycięstwo odniósł gracz X!";
+        else message = "Zwycięztwo odniósł gracz Y!";
+        score = "Aktualny wynik   "+scoreX+" : "+scoreY;
+        board.winBoard(cells,message,score);
 
 
 
     }
 
     void translator(String answer){
-        if (answer.equals("A1")){x=0; y=0;}
-        else if (answer.equals("A2")){x=1; y=0;}
-        else if (answer.equals("A3")){x=2; y=0;}
-        else if (answer.equals("B1")){x=0; y=1;}
-        else if (answer.equals("B2")){x=1; y=1;}
-        else if (answer.equals("B3")){x=2; y=1;}
-        else if (answer.equals("C1")){x=0; y=2;}
-        else if (answer.equals("C2")){x=1; y=2;}
-        else if (answer.equals("C3")){x=2; y=2;}
+        answer = answer.toLowerCase();
+        if (answer.equals("a1")){x=0; y=0;}
+        else if (answer.equals("a2")){x=1; y=0;}
+        else if (answer.equals("a3")){x=2; y=0;}
+        else if (answer.equals("b1")){x=0; y=1;}
+        else if (answer.equals("b2")){x=1; y=1;}
+        else if (answer.equals("b3")){x=2; y=1;}
+        else if (answer.equals("c1")){x=0; y=2;}
+        else if (answer.equals("c2")){x=1; y=2;}
+        else if (answer.equals("c3")){x=2; y=2;}
         else System.out.println("zle");
     }
 
