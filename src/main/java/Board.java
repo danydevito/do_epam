1dc5b29ce38fc5a7c188b140929f4492301e43fe
danydevito <daniel1421@vp.pl>
2017-05-22 11:50:32 +0200

    "create Board"

---
+++ src/main/java/Board.java
@@ -0,0 +1,16 @@
+/**
+ * Created by daniel on 2017-05-22.
+ */
+public class Board {
+
+    public void drawBoard(String[][] xFields, String[][] yFields ){
+        System.out.println("     A   B   C");
+        System.out.println("    --- --- --- ");
+        System.out.println(" 1 |   |   |   |");
+        System.out.println("    --- --- ---");
+        System.out.println(" 2 |   |   |   |");
+        System.out.println("    --- --- ---");
+        System.out.println(" 3 |   |   |   |");
+        System.out.println("    --- --- ---");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Created by daniel on 2017-05-22.
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello Epam!");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 1;
+        String[][] xFields = new String[3][3];
+        String[][] yFields = new String[3][3];
+
+        System.out.println("Kółko krzyżyk");
+        System.out.println();
+        while (choice != 2){
+
+            System.out.println("MENU");
+            System.out.println("___Nowa gra___ (1)");
+            System.out.println("___Zakończ____ (2)");
+            System.out.println("Wybierz opcję i zatwierdz klikając ENTER: ");
+            choice = scanner.nextInt();
+
+            if (choice==1){
+                Board board = new Board();
+                board.drawBoard(xFields, yFields);
+            }else if (choice==2){
+                System.out.println("Koniec.");
+            }else {
+                System.out.println("Niewłaściwa opcja! Spróbuj ponownie");
+            }
+        }
     }
 }
